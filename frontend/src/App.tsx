5e2a2f8c7bd1718509eabb51a9dec8786e625ee3
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-02-24 08:29:29 +0300

    fixed rrd issues

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Grid, Typography } from "@mui/material";
 import { AppBar, Button } from "./components";
 import Landing from "./pages/Home/Landing";
-import man from "./assets/man.svg";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Landing />,
-  },
-]);
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <AppBar />
-      <RouterProvider router={router} />
+      <Landing />
+      <Contact />
     </div>
   );
 }
