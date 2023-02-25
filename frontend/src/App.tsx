26d2ce0eafca6702299c63b8a7e824e9bb10b27d
Brian Mwendwa <brianmwendwa.mu@gmail.com>
2023-02-25 19:59:36 +0300

    update contact

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import { AppBar, Button } from "./components";
 import Landing from "./pages/Home/Landing";
-import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <AppBar />
       <Landing />
-      <Contact />
     </div>
   );
 }
--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -4,3 +4,4 @@ export { default as Container } from "./Container";
 export { default as Section } from "./Section";
 export { default as Modal } from "./Modal";
 export { default as List } from "./List";
+export { default as Contact } from "./Contact";
